f4de04fca78b787f2ce8f1e8b6871b869ee98d89
cococrazyy <119519291+cococrazyy@users.noreply.github.com>
2026-02-27 10:28:22 -0600

    testing to see if Reid can be added to authorized users

--- app.py
+++ app.py
@@ -177,9 +177,9 @@ def admin_page():
 @app.route("/seed-admin")
 def seed_admin():
     
-    email = "garza22@southwestern.edu"   # must match Google email
-    first_name = "Aaron"
-    last_name = "Garza"
+    email = "williamsr@southwestern.edu"   # must match Google email
+    first_name = "Reid"
+    last_name = "Williams"
     
     volunteer = Volunteer.query.filter_by(email=email).first()
     if not volunteer:
