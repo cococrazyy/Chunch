9b58e2ce5ada15066449bb3dea0986c2eee0822f
cococrazyy <119519291+cococrazyy@users.noreply.github.com>
2026-04-05 22:39:48 -0500

    working on getting the hours added correctly after accepting from inbox

--- app.py
+++ app.py
@@ -1840,12 +1840,12 @@ def accept_applicant():
     if end_hour <= start_hour:
         return "Invalid time range", 400
         
-    for hour in range(start_hour, end_hour):
+    for hour in range(start_hour, end_hour+1):
         availability = Availability(
             volunteer_id=volunteer.id,
             hour=hour
-    )
-    db.session.add(availability)
+        )
+        db.session.add(availability)
     
     assignment = Assignment(
         volunteer_id=volunteer.id,
