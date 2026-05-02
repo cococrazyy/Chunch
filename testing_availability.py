c1993a7f226b79fb1e574f03b4fdcfd431e5352b
Eleanor <123319855+WagnerEC1106@users.noreply.github.com>
2026-05-02 16:11:52 +0000

    Remove unused file

--- testing_availability.py
+++
@@ -1,9 +0,0 @@
-from app import db, Volunteer, Availability, app
-
-with app.app_context():
-    v = Volunteer.query.first()
-    print(v.first_name, v.last_name)
-    if v.availability:
-        print(v.availability[0].hour)  # only first hour
-    else:
-        print("No availability")
